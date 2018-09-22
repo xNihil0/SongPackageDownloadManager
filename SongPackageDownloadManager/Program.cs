52e7f2ed30c30090a501c322d714926926e738bf
xNihil0 <CSAdvenT@gmail.com>
2018-09-23 03:10:27 +0800

    Start from JSON support.

---
+++ SongPackageDownloadManager/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SongPackageDownloadManager
+{
+    public class SongAttributes
+    {
+        public string name { get; set; }
+        public double average { get; set; }
+        public string download { get; set; }
+        public int size { get; set; }
+    }
+
+    public class SongDetail
+    {
+        public string type { get; set; }
+        public string id { get; set; }
+        public SongAttributes attributes { get; set; }
+    }
+
+    public class PackageList
+    {
+        public List<SongDetail> data { get; set; }
+    }
+
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmMain());
+        }
+    }
+}
---
+++ SongPackageDownloadManager/frmMain.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+
+namespace SongPackageDownloadManager
+{
+    public partial class frmMain : Form
+    {
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetPkgLst_Click(object sender, EventArgs e)
+        {
+            WebRequest request = WebRequest.Create("https://etternaapi.xnihilo.live/v2/packs");
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string rawjson = reader.ReadToEnd();
+            reader.Close();
+            response.Close();
+
+            PackageList packageList = JsonConvert.DeserializeObject<PackageList>(rawjson);
+            List<SongDetail> songDetail = packageList.data;
+            StringBuilder sb = new StringBuilder();
+            foreach (SongDetail detail in songDetail)
+            {
+                SongAttributes songAttributes = detail.attributes;
+                sb.AppendLine(string.Format("Pack Name: {0} Average Difficulty: {1}  Size: {2}", songAttributes.name, songAttributes.average, songAttributes.size));
+            }
+            textBox1.Text = sb.ToString();
+        }
+    }
+}
